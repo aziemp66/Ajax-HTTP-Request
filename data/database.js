146c06fbf2d7e65e3348351f33a69819cdd6f545
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-16 23:08:15 +0700

    Adding adjustment

--- data/database.js
+++ data/database.js
@@ -1,22 +1,22 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 
 let database;
 
 async function connect() {
-  const client = await MongoClient.connect('mongodb://localhost:27017');
-  database = client.db('blog');
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    database = client.db("blog");
 }
 
 function getDb() {
-  if (!database) {
-    throw { message: 'Database connection not established!' };
-  }
-  return database;
+    if (!database) {
+        throw { message: "Database connection not established!" };
+    }
+    return database;
 }
 
 module.exports = {
-  connectToDatabase: connect,
-  getDb: getDb
+    connectToDatabase: connect,
+    getDb: getDb,
 };
