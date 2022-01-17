b0f4d0a981728f145618eb1b2ea228cce2bba42e
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-17 19:37:28 +0700

    Updating DOM Based on the response

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,10 +1,31 @@
 const loadCommentsBtnElement = document.getElementById("load-comments-btn");
+const commentSectionElement = document.getElementById("comments");
+
+function createCommentsList(comments) {
+    const commentListsElement = document.createElement("ol");
+
+    for (const comment of comments) {
+        const commentElement = document.createElement("li");
+        commentElement.innerHTML = `
+        <article class="comment-item">
+            <h2>${comment.title}</h2>
+            <p>${comment.text}</p>
+        </article>
+        `;
+        commentListsElement.appendChild(commentElement);
+    }
+
+    return commentListsElement;
+}
 
 async function fetchCommentsForPosts() {
     const postId = loadCommentsBtnElement.dataset.postid;
     const response = await fetch(`/posts/${postId}/comments`);
     const responseData = await response.json();
-    console.log(responseData);
+
+    const commentsListElement = createCommentsList(responseData);
+    commentSectionElement.innerHTML = "";
+    commentSectionElement.appendChild(commentsListElement);
 }
 
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPosts);
