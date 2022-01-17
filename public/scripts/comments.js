579638f6d32013f62d8e33bd8b431fd5834abdb7
Azie Melza Pratama <aziemp66@gmail.com>
2022-01-17 22:46:59 +0700

    Improving User Experiences

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -26,12 +26,17 @@ async function fetchCommentsForPosts() {
     const response = await fetch(`/posts/${postId}/comments`);
     const responseData = await response.json();
 
-    const commentsListElement = createCommentsList(responseData);
-    commentSectionElement.innerHTML = "";
-    commentSectionElement.appendChild(commentsListElement);
+    if (responseData && responseData.length > 0) {
+        const commentsListElement = createCommentsList(responseData);
+        commentSectionElement.innerHTML = "";
+        commentSectionElement.appendChild(commentsListElement);
+    } else {
+        commentSectionElement.firstElementChild.textContent =
+            "We could not find any comments. Maybe add one?";
+    }
 }
 
-function saveComment(event) {
+async function saveComment(event) {
     event.preventDefault();
     const postId = commentsFormElement.dataset.postid;
 
@@ -40,13 +45,15 @@ function saveComment(event) {
 
     const comment = { title: enteredTitle, text: enteredText };
 
-    fetch(`/posts/${postId}/comments`, {
+    const response = await fetch(`/posts/${postId}/comments`, {
         method: "POST",
         body: JSON.stringify(comment),
         headers: {
             "Content-Type": "application/json",
         },
     });
+
+    fetchCommentsForPosts();
 }
 
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPosts);
